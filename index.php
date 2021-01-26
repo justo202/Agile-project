ecf0ec3eb50214e69742d2d0e541048e35092a03
justo202 <justaslabeikis23@gmail.com>
2021-01-26 04:53:56 +0200

    Update index.php

--- index.php
+++ index.php
@@ -26,6 +26,30 @@ if (!$link) {
 
 echo "Success: A proper connection to MySQL was made! The my_db database is great." . PHP_EOL;
 echo "Host information: " . mysqli_get_host_info($link) . PHP_EOL;
+$sql = "SELECT * FROM test";
+if($result = mysqli_query($link, $sql)){
+    if(mysqli_num_rows($result) > 0){
+        echo "<table>";
+            echo "<tr>";
+                echo "<th>id</th>";
+                  echo "<th>issd</th>";
+                    echo "<th>idsss</th>";
+                      echo "<th>idsss</th>";
 
+            echo "</tr>";
+        while($row = mysqli_fetch_array($result)){
+            echo "<tr>";
+                echo "<td>" . $row['item1'] . "</td>";
+                echo "<td>" . $row['item2'] . "</td>";
+                echo "<td>" . $row['item3'] . "</td>";
+                echo "<td>" . $row['item4'] . "</td>";
+            echo "</tr>";
+        }
+        echo "</table>";
+        // Free result set
+        mysqli_free_result($result);
+    } else{
+        echo "No records matching your query were found.";
+    }
 mysqli_close($link);
 ?>
