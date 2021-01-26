bd828aeece3a4f1d857a5c4ac0dde118ec241483
justo202 <justaslabeikis23@gmail.com>
2021-01-26 15:29:17 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,33 +1,33 @@
-<?php
-include 'db.php';
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Questionaire</title>
+  </head>
+  <body>
+    <?php
+    include 'db.php';
 
-echo "Success: A proper connection to MySQL was made! The my_db database is great." . PHP_EOL;
-echo "Host information: " . mysqli_get_host_info($link) . PHP_EOL;
 
-$sql = "SELECT * FROM `user`";
-if($result = mysqli_query($link, $sql)){
-    if(mysqli_num_rows($result) > 0){
-        echo "<table>";
-            echo "<tr>";
-                echo "<th>id</th>";
-                  echo "<th>issd</th>";
-                    echo "<th>idsss</th>";
-                      echo "<th>idsss</th>";
 
-            echo "</tr>";
-        while($row = mysqli_fetch_array($result)){
-            echo "<tr>";
-                echo "<td>" . $row['Username'] . "</td>";
-                echo "<td>" . $row['Password'] . "</td>";
-                echo "<td>" . $row['Role'] . "</td>";
-            echo "</tr>";
+    $sql = "SELECT * FROM `questions` WHERE `Questionnaire_Name` = `test`";
+    if($result = mysqli_query($link, $sql)){
+        if(mysqli_num_rows($result) > 0){
+          echo "Questionaire starts now: "
+          echo "<br>"
+
+            while($row = mysqli_fetch_array($result)){
+
+                    echo $row['Question'];
+            }
+            echo "</table>";
+            // Free result set
+            mysqli_free_result($result);
+        } else{
+            echo "No records matching your query were found.";
         }
-        echo "</table>";
-        // Free result set
-        mysqli_free_result($result);
-    } else{
-        echo "No records matching your query were found.";
     }
-}
-mysqli_close($link);
-?>
+    mysqli_close($link);
+    ?>
+  </body>
+</html>
