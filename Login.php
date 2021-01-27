f7470ad705b683c49898282093b59ad198c2f2b0
vivienprk <77679471+vivienprk@users.noreply.github.com>
2021-01-27 17:44:28 +0100

    Update Login.php

--- Login.php
+++ Login.php
@@ -1,3 +1,83 @@
+<?php
+
+session_start();
+if(isset($_SESSION["loggedin"])&&$_SESSION["loggedin"]==true){
+    header("location: test.php");
+    exit;
+}
+
+
+// Include config file
+require_once "db.php";
+
+$email = $password = "";
+$email_err = $password_err = "";
+
+// Processing form data when form is submitted
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+ 
+    // Validate email
+    if(empty(trim($_POST["email"]))){
+        $email_err = "Please enter your email.";
+    }else
+    {
+        $email = trim($_POST["email"]);
+    }
+
+    // Validate password
+    if(empty(trim($_POST["password"]))){
+        $password_err = "Please enter your password.";     
+    } else{
+        $password = trim($_POST["password"]);
+    }
+    
+    // Check input errors before inserting in database
+    if(empty($email_err) && empty($password_err)){
+        
+        // Prepare an Select statement
+        $sql = "SELECT ID, email, password FROM participant WHERE email = :email";
+         
+        if($stmt = $mysql->prepare($sql)){
+            // Bind variables to the prepared statement as parameters
+            $stmt->bindParam(":email", $param_email, PDO::PARAM_STR);           
+            // Set parameters
+            $param_email = $email;           
+            // Attempt to execute the prepared statement
+            if($stmt->execute()){
+                if ($stmt->rowCount()==1) {
+                    if ($row = $stmt->fetch()) {
+                        $id = $row["ID"];
+                        $email = $row["email"];
+                        $hashed_password = $row["password"];
+                        //if password matches start the session
+                        if ($password == $hashed_password) {
+                            session_start();
+                            //store data in session variables 
+                            $_SESSION["loggedin"] = true;
+                            $_SESSION["id"] = $id;
+                            $_SESSION["email"] = $email;
+
+                            header("location: test.php");
+                        }else{
+                            $password_err = "The password you entered was not valid";
+                        }
+                    }
+                }else{
+                    $email_err = "No account found with that username" ;
+                }
+            } else{
+                echo "Something went wrong. Please try again later.";
+            }
+
+            // Close statement
+            unset($stmt);
+        }
+    }
+    // Close connection
+    unset($mysql);
+}
+?>
+
 <!doctype html>
 <html lang="en">
   <head>
