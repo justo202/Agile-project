94267f7a3cb85912e45a0495f35fd16fcd6fd1bb
justo202 <justaslabeikis23@gmail.com>
2021-01-27 18:10:36 +0200

    Update answerss.php

--- answerss.php
+++ answerss.php
@@ -2,7 +2,7 @@
 $answers = $_POST['answers'];
 $question = $_POST['Question'];
 $number = $_POST['num'];
-$i = 0
+$i = 0;
 foreach($answers as $answer)
 {
 echo $answer." ".$question[$i]." ".$number[$i]."<br>";
