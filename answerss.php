37af48678106d4e7209664bf606c3823ac601d10
justo202 <justaslabeikis23@gmail.com>
2021-01-27 18:37:30 +0200

    Update answerss.php

--- answerss.php
+++ answerss.php
@@ -9,11 +9,13 @@ $i = 0;
 foreach($answers as $answer)
 {
   echo $answer." ".$name." ".$number[$i]."<br>";
-  $sql = "INSERT INTO `answers` (`Question_Number`, `Questionnaire_Name`, `Answer`) VALUES ('1', '$name', '$answer');";
-  if(!$result = mysqli_query($link, $sql))
-  {
-    echo "something went wrong";
-  }
+  $sql = "INSERT INTO `answers` (`Question_Number`, `Questionnaire_Name`, `Answer`) VALUES ('1', '$name', '$answer')";
+
+  if ($link->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $link->error;
+}
   $i++;
 }
 mysqli_close($link);
