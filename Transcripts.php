48da5c98fb7c72ff0756f8d41d2967066c1ae543
Brenyn Goodwin <44468754+LupusSolus99@users.noreply.github.com>
2021-01-27 15:07:14 +0000

    Add files via upload

--- Transcripts.php
+++ Transcripts.php
@@ -5,24 +5,23 @@
 </head>
 
 <body>
-<form action="getmethod.php" method="get"> <input type="text" name="Transcript" placeholder="Enter text here" /> <input type="submit" name="submit" /> </form>
+<form method="post"> <input type="text" name="Transcript" placeholder="Enter text here" /> <input type="submit" name="submit" /> </form>
+
+</body>
+</html>
 
 <?php
 //check if the form is submitted  
-if ( isset( $_GET['submit'] ) )
+if (isset($_POST['submit'] ) )
 	{ //retrieve the data in the form
-	$txt = $_GET['Transcript'];
+	$txt = $_POST['Transcript'];
 	
 	//display the results
 	echo $txt;
 	
 	//write to the file
-	$transcript = fopen("transcript.txt", "w") or die("Unable to open file!")
-	$txt = $_GET['Transcript'];
+	$transcript = fopen("transcript.txt", "a") or die("Unable to open file!");
 	fwrite($transcript, $txt);
 	fclose($transcript);
 	}
 ?>
-	
-</body>
-</html>
