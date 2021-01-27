dc9a872fdb318b87abd8bdc6dfbaa5f99f3f284a
justo202 <justaslabeikis23@gmail.com>
2021-01-27 17:58:45 +0200

    test

--- answerss.php
+++ answerss.php
@@ -1,7 +1,7 @@
 <?php
 $answers = $_POST['answers'];
 $question = $_POST['Question'];
-$number = $_POST['Number'];
+$number = $_POST['num'];
 echo $answers[1]."<br>".$question[1]."<br>".$number[1];
 echo $number[1];
 
