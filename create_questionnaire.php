026bcdc8415c64f41a1ae76238b61ce6cce1add9
dmcgraw14 <mcgraw.dominic14@gmail.com>
2021-01-27 11:11:18 +0000

    Update create_questionnaire.php

--- create_questionnaire.php
+++ create_questionnaire.php
@@ -21,7 +21,7 @@ $questions_array = get_questions($num_of_questions);
 
 function add_questionaire($questionnaire_name, $creator_name, $link)
 {
-    $add_questionnaire_sql = "INSERT INTO questionnaires VALUES (".$questionnaire_name.", ".$creator_name.")";
+    $add_questionnaire_sql = "INSERT INTO questionnaires VALUES ('$questionnaire_name', '$creator_name')";
     if ($link->query($add_questionnaire_sql) === TRUE)
     {
         echo "Questionnaire ".$questionnaire_name." created successfully <br>";
@@ -34,7 +34,7 @@ function add_questions($questions_array, $questionnaire_name, $num_of_questions,
 {
     for($x = 0; $x < $num_of_questions; $x++)
     {
-        $add_question_sql = "INSERT INTO 'questions' VALUES (".$question_array[$x].", ".$questionnaire_name.")";
+        $add_question_sql = "INSERT INTO 'questions' VALUES ('$question_array[$x]', '$questionnaire_name')";
         if ($link->query($add_questionnaire_sql) === TRUE)
         {
             echo $question_array[$x]."<br> added to questionnaire <br>";
