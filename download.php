48df61a6f90a2c9241e3ce6b7d5a182a83f9aff0
justo202 <justaslabeikis23@gmail.com>
2021-01-28 13:55:47 +0200

    download done

--- download.php
+++ download.php
@@ -2,27 +2,35 @@
   include 'db.php';
   $name = $_GET['name'];
   $myfile = fopen("data.txt", "w") or die("Unable to open file!");
+  header('Content-Type: application/download');
+  header('Content-Disposition: attachment; filename="data.txt"');
+  header("Content-Length: " . filesize("data.txt"));
+  fpassthru($myfile);
+
   $sql = "SELECT * FROM `answers` WHERE `Questionnaire_Name` = '$name' ORDER BY `Question_Number` ASC";
   if($result = mysqli_query($link, $sql)){
       if(mysqli_num_rows($result) > 0){
-        fwrite($myfile, "'$name'\n");
+        echo "$name\n";
+        $num = " ";
           while($row = mysqli_fetch_array($result)){
-            $txt = "Answers to question 1";
-            fwrite($myfile, $txt);
+            if($num != $row['Question_Number'])
+              {
+              echo "\nQuestion ".$row['Question_Number'].".\n";
+              $num = $row['Question_Number'];
+              }
+            echo "- ".$row['Answer']."\n";
           }
           // Free result set
           mysqli_free_result($result);
       } else{
-          $txt = "No data found for this survey";
-          fwrite($myfile, $txt);
+        echo "Failed to find data for the survey";
       }
-header('Content-Type: application/download');
-header('Content-Disposition: attachment; filename="data.txt"');
-header("Content-Length: " . filesize("data.txt"));
+
 //$fp = fopen("data.txt", "r");
-fpassthru($myfile);
+
 echo "hello, this is a test";
-fclose($myfile);
+
   }
+  fclose($myfile);
   mysqli_close($link);
  ?>
