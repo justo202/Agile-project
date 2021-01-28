ddb4754664a8b563d2f548941981b65c7303d8ef
dmcgraw14 <mcgraw.dominic14@gmail.com>
2021-01-28 15:07:20 +0000

    Update view_answers.php

--- view_answers.php
+++ view_answers.php
@@ -155,7 +155,7 @@ print_r($results_arr);
 
             <?php
 
-            for($x=sizeof($results_arr); $x > 0; $x--)
+            for($x=0; $x > sizeof($results_arr); $x++)
             {
                   echo "hello im working";
                   echo "<label>Question ". $results_arr[$x]->view_question_num() .":</label><br>";
