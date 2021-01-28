e0e70f93763327175a63e7fe94dd757103c98f3e
dmcgraw14 <mcgraw.dominic14@gmail.com>
2021-01-28 11:20:00 +0000

    Update view_answers.php

--- Project/view_answers.php
+++ Project/view_answers.php
@@ -7,15 +7,38 @@ $questionnaire_name = 'Test Questionnaire';
 
 function getAnswers($questionnaire_name, $link)
 {
-    $get_answers_sql = "SELECT Question_Number, Answer FROM answers WHERE Questionnaire_Name = 'Test Questionnaire'";
-    if ($link->query($get_answers_sql) === TRUE)
+    $get_answers_sql = "SELECT Question_Number, Answer FROM answers WHERE Questionnaire_Name = '".$questionnaire_name."'";
+    $results = $link->query($get_answers_sql);
+
+    if ($results->num_rows > 0)
     {
+        return $results;
         echo "successfully retrieved answers<br>";
-    } else {
+    } 
+    else 
+    {
+        echo "Error: <br>" . $link->error;
+    }
+}
+
+
+function getQuestions($questionnaire_name, $link)
+{
+    $get_questions_sql = "SELECT Question_Number, Question FROM questions WHERE Questionnaire_Name = '".$questionnaire_name."'";
+    $results = $link->query($get_questions_sql);
+
+    if ($results->num_rows > 0)
+    {
+        return $results;
+        echo "successfully retrieved questions<br>";
+    } 
+    else 
+    {
         echo "Error: <br>" . $link->error;
     }
 }
 
 getAnswers($questionnaire_name, $link);
+getQuestions($questionnaire_name, $link);
 
 ?>
