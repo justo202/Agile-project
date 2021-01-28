8e3d767dcf92551b0b664276f5f53e2680eedd2d
gerardoblanco <gerardblanc.oh@gmail.com>
2021-01-28 15:34:51 +0100

    Log out added

--- Form.php
+++ Form.php
@@ -23,7 +23,7 @@
               <span class="navbar-toggler-icon"></span>
             </button>
 
-            <a class="navbar-brand" href="Login.php">Login</a>
+            <a class="navbar-brand" href="index.php">Log out</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
               <span class="navbar-toggler-icon"></span>
             </button>
