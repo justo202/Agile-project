65d5cdc13774f4010e116bedeae81338e316b33b
gerardoblanco <gerardblanc.oh@gmail.com>
2021-01-28 21:27:53 +0100

    fix for displaying countdown

--- create_questionnaire.php
+++ create_questionnaire.php
@@ -1,48 +1,4 @@
-<!doctype html>
-<html lang="en">
-  <head>
 
-    <title>Home</title>
-
-    <!-- Style Links -->
-    <link rel="stylesheet" href="https://unpkg.com/bootstrap-material-design@4.1.1/dist/css/bootstrap-material-design.min.css">
-
-  </head>
-
-  <body>
-    <!-- Display the countdown timer in an element -->
-    <p id="demo"></p>
-
-    <script>
-      // Set the date we're counting down to
-      var countDownDate = new Date().getTime() +3;
-
-      // Update the count down every 1 second
-      var x = setInterval(function() {
-
-        // Get today's date and time
-        var now = new Date().getTime();
-
-        // Find the distance between now and the count down date
-        var distance = countDownDate - now;
-
-        // Time calculations for days, hours, minutes and seconds
-        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-        // Display the result in the element with id="demo"
-        document.getElementById("demo").innerHTML = days + "d " + hours + "h "
-        + minutes + "m " + seconds + "s ";
-
-        // If the count down is finished, write some text
-        if (distance < 0) {
-          clearInterval(x);
-          document.getElementById("demo").innerHTML = "EXPIRED";
-        }
-      }, 1000);
-    </script>
 
 
     <?php
@@ -98,6 +54,51 @@
 
     ?>
 
+    <!doctype html>
+    <html lang="en">
+      <head>
+
+        <title>Create Questionaire</title>
+
+        <!-- Style Links -->
+        <link rel="stylesheet" href="https://unpkg.com/bootstrap-material-design@4.1.1/dist/css/bootstrap-material-design.min.css">
+
+      </head>
+
+      <body>
+        <!-- Display the countdown timer in an element -->
+        <p id="demo"></p>
+
+        <script>
+          // Set the date we're counting down to
+          var countDownDate = new Date().getTime() +3;
+
+          // Update the count down every 1 second
+          var x = setInterval(function() {
+
+            // Get today's date and time
+            var now = new Date().getTime();
+
+            // Find the distance between now and the count down date
+            var distance = countDownDate - now;
+
+            // Time calculations for days, hours, minutes and seconds
+            var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+            var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+            var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+            // Display the result in the element with id="demo"
+            document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+            + minutes + "m " + seconds + "s ";
+
+            // If the count down is finished, write some text
+            if (distance < 0) {
+              clearInterval(x);
+              document.getElementById("demo").innerHTML = "EXPIRED";
+            }
+          }, 1000);
+        </script>
 
   </body>
 </html>
