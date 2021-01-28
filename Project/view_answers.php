8a9c1c6f46437337a16a1d76e1fb14076885b440
dmcgraw14 <mcgraw.dominic14@gmail.com>
2021-01-28 11:10:07 +0000

    Update view_answers.php

--- Project/view_answers.php
+++ Project/view_answers.php
@@ -4,11 +4,10 @@ include 'db.php';
 
 // $questionnaire_name = $_POST['questionnaire_name'];
 $questionnaire_name = 'Test Questionnaire';
-echo 'im working';
 
 function getAnswers($questionnaire_name, $link)
 {
-    $get_answers_sql = "SELECT Question_Number, Answer FROM answers WHERE Questionnaire_Name = '".$questionnaire_name."'";
+    $get_answers_sql = "SELECT Question_Number, Answer FROM answers WHERE Questionnaire_Name = 'Test Questionnaire'";
     if ($link->query($get_answers_sql) === TRUE)
     {
         echo "successfully retrieved answers<br>";
