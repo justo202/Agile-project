0f19cb18d38663eed2b75b6537244f6d0a7b2153
Brenyn Goodwin <44468754+LupusSolus99@users.noreply.github.com>
2021-01-28 14:37:13 +0000

    Add files via upload

--- Transcripts.php
+++ Transcripts.php
@@ -17,10 +17,25 @@
 </html>
 
 <?php
+$dir = "Uploads/";
 //varaible that contains details fo files
-$fileName = basename($_FILES["file"]["name"]);
+$fileName = $dir . basename($_FILES["file"]["name"]);
 //locates the file
-$fileType = strtolower(pathinfo($filename, PATHINFO_EXTENSION));
+$fileType = strtolower(pathinfo($fileName, PATHINFO_EXTENSION));
+
+//Check file type
+
+if (isset($_POST["submit"]))
+{
+	if ($fileType != "txt")
+	{
+		echo "Wrong file type, please ensure the file is a txt file.";
+	}
+	else
+	{
+		echo "Your file has been uploaded! (Just believe me it is there, just invisibile)";
+	}
+}
  
 if (isset($_POST["Transcript"]))
 { //retrieve the data in the form
@@ -33,17 +48,4 @@ if (isset($_POST["Transcript"]))
 	fclose($transcript);
 }
 	
-//Check file type
-
-if (isset($_POST["submit"]))
-{
-	if ($fileType != "txt")
-	{
-		echo "Wrong file type, Please ensure the file is a txt file.";
-	}
-	else
-	{
-		echo "Your file has been uploaded! (Just believe me it is there, just invisibile)";
-	}
-}	
 ?>
