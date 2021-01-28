806db1f0e5ddff761855bcd2a667256ce5ead4bb
dmcgraw14 <mcgraw.dominic14@gmail.com>
2021-01-28 14:18:37 +0000

    add answers.html

--- view_answers.php
+++ view_answers.php
@@ -37,9 +37,9 @@ class results {
         echo $this->question;
     }
 
-    function view_answers()
+    function view_answer($i)
     {
-        print_r($this->answers_arr);
+        echo $this->answers_arr[$i];
     }
 }
 
@@ -97,3 +97,97 @@ while($row = $answer_rows->fetch_assoc())
 }
 
 ?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+
+    <title>Anwers</title>
+
+    <!-- Style Links -->
+    <link rel="stylesheet" href="https://unpkg.com/bootstrap-material-design@4.1.1/dist/css/bootstrap-material-design.min.css">
+
+  </head>
+
+  <body>
+
+    <header>
+      <header>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <a class="navbar-brand" href="Index.html">Home</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <a class="navbar-brand" href="Login.html">Login</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <a class="navbar-brand" href="Form.html">Form</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <a class="navbar-brand" href="CreateForm.html">Create Form</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+    </header>
+
+    <!-- Begin page content -->
+    <main role="main" class="container">
+        <h1 class="mt-5">View Answers</h1>
+        <p class="lead">Below you can choose the results you want to see</p>
+
+        <form action="view_answers.php" method="post">
+          <label>Enter Name of Questionnaire:</label>
+          <input type="text" id="questionnaire_name" name="questionnaire_name">
+          <input type="submit" value="Get Results" class="btn btn-primary"> 
+        </form>
+      
+      <form class="form-group">
+        <div class="border border-primary rounded">
+
+            <?php
+
+            for($x=0; $x < sizeof($results_arr); $x++)
+            {
+                ?>  <label>Question <?php $results_arr[$x]->view_question_num() ?>:</label><br>
+                    <small id="Question"><?php $results_arr[$x]->view_question() ?></small>
+                    <br><br>
+
+                    <?php 
+                        for($i=0; $i < sizeof($results_arr[$answers_arr]); $i++)
+                        {
+                            ?>
+                            <label>Answer <?php echo $i+1 ?>:</label><br>
+                            <small id="Answer"><?php $results_arr[$x]->view_answer($i) ?></small>
+                            <?php
+                        }
+            }
+            ?>
+            
+            <!-- <label>Question 1:</label><br>
+            <small id="Question">Eample Question?</small>
+            <br><br>
+            <label>Answer 1:</label><br>
+            <small id="Answer">Eample Answer.</small> -->
+
+        </div>
+
+        <br>
+        <input type="submit" value="Export" class="btn btn-primary btn-outline-primary">
+
+      </form>
+
+    </main>
+
+     <!--<footer class="footer">
+      <div class="container">
+        <span class="text-muted">Place sticky footer content here.</span>
+      </div>
+    </footer> -->
+
+  </body>
+</html>
