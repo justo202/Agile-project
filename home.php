75c98ac1e04b87e89cd0786da8871291aeed6ee2
gerardoblanco <gerardblanc.oh@gmail.com>
2021-01-28 15:32:07 +0100

    Removed dead login link

--- home.php
+++ home.php
@@ -20,11 +20,6 @@
           <span class="navbar-toggler-icon"></span>
         </button>
 
-        <a class="navbar-brand" href="Login.php">Login</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-
         <a class="navbar-brand" href="Form.php">Form</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
               <span class="navbar-toggler-icon"></span>
