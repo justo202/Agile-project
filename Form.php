23ca89e26b6208e1719ebb164d72711182747ba5
gerardoblanco <gerardblanc.oh@gmail.com>
2021-01-28 15:39:34 +0100

    Removed navbar link to form.php Added temporary 'Log out' which redirects to index.php

--- Form.php
+++ Form.php
@@ -27,12 +27,6 @@
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
               <span class="navbar-toggler-icon"></span>
             </button>
-
-            <a class="navbar-brand" href="Form.php">Form</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon"></span>
-            </button>
-            </nav>
     </header>
 
     <!-- Begin page content -->
