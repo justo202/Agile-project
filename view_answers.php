2a09926f83bc5a8d0f80eb5b384b5e185cecb315
gerardoblanco <gerardblanc.oh@gmail.com>
2021-01-29 11:20:37 +0100

    syntax error fix

--- view_answers.php
+++ view_answers.php
@@ -136,6 +136,7 @@ while($row = $question_rows->fetch_assoc())
     <header>
       <header>
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+          <a class="navbar-brand" href="home.php">Home</a>
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
