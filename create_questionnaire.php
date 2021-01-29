52e10b03345d6023313280f252394e40a5b5ce3f
gerardoblanco <gerardblanc.oh@gmail.com>
2021-01-29 11:34:19 +0100

    fix error message display

--- create_questionnaire.php
+++ create_questionnaire.php
@@ -1,69 +1,3 @@
- <?php
-    include 'db.php';
-    header( "refresh:6;url=https://agile-project.azurewebsites.net/home.php" );
-    $questionnaire_name = $_POST['questionnaire_name'];
-    $creator_name = $_POST['creator_name'];
-    $num_of_questions = $_POST['num_of_questions'];
-
-    //Getting the questions that the user has created and storing them in an array
-    function get_questions($num_of_questions)
-    {
-        //Create the array
-        $questions = array();
-
-        for($x = 1; $x <= $num_of_questions; $x++)
-        {
-            //Add each question to the the array
-            $questions[$x] = $_POST['question'.$x];
-        }
-
-        //Return the filled array
-        return $questions;
-    }
-
-    $questions_array = get_questions($num_of_questions);
-
-    //This function creates new records in the questionnaire table in the database
-    //It will add the name of the questionnaire and the name of the user who created it
-    function add_questionaire($questionnaire_name, $creator_name, $link)
-    {
-        //Store the sql statement in variable $add_questionnaire_sql
-        $add_questionnaire_sql = "INSERT INTO questionnaires VALUES ('".$questionnaire_name."', '".$creator_name."')";
-
-        if ($link->query($add_questionnaire_sql) === TRUE)
-        {
-            echo "Questionnaire ".$questionnaire_name." created successfully <br>";
-        } else {
-            echo "Error: " . $add_questionnaire_sql . "<br>" . $link->error;
-        }
-    }
-
-    //This function creates new records in the question table in the database
-    //It will add the name of the questionnaire and each question in the questionnaire
-    function add_questions($questions_array, $questionnaire_name, $num_of_questions, $link)
-    {
-        //each question in the questions array will need to be added so the next bit of code is
-        //iterates through the array.
-        for($x = 1; $x <= $num_of_questions; $x++)
-        {
-            //Store the sql statement in variable $add_question_sql
-            $add_question_sql = "INSERT INTO questions VALUES ('".$questions_array[$x]."', '".$questionnaire_name."', '".$x."')";
-            if ($link->query($add_question_sql) === TRUE)
-            {
-                echo $question_array[$x]."<br> added to questionnaire <br>";
-            } else {
-                echo "Error: " . $add_question_sql . "<br>" . $link->error;
-            }
-        }
-    }
-
-    add_questionaire($questionnaire_name, $creator_name, $link);
-    add_questions($questions_array, $questionnaire_name, $num_of_questions, $link);
-
-
-    ?>
-
-
 <!doctype html>
 <html lang="en">
   <head>
@@ -93,6 +27,71 @@
 
     <!-- Begin page content -->
     <main role="main" class="container">
+      <?php
+         include 'db.php';
+         header( "refresh:6;url=https://agile-project.azurewebsites.net/home.php" );
+         $questionnaire_name = $_POST['questionnaire_name'];
+         $creator_name = $_POST['creator_name'];
+         $num_of_questions = $_POST['num_of_questions'];
+
+         //Getting the questions that the user has created and storing them in an array
+         function get_questions($num_of_questions)
+         {
+             //Create the array
+             $questions = array();
+
+             for($x = 1; $x <= $num_of_questions; $x++)
+             {
+                 //Add each question to the the array
+                 $questions[$x] = $_POST['question'.$x];
+             }
+
+             //Return the filled array
+             return $questions;
+         }
+
+         $questions_array = get_questions($num_of_questions);
+
+         //This function creates new records in the questionnaire table in the database
+         //It will add the name of the questionnaire and the name of the user who created it
+         function add_questionaire($questionnaire_name, $creator_name, $link)
+         {
+             //Store the sql statement in variable $add_questionnaire_sql
+             $add_questionnaire_sql = "INSERT INTO questionnaires VALUES ('".$questionnaire_name."', '".$creator_name."')";
+
+             if ($link->query($add_questionnaire_sql) === TRUE)
+             {
+                 echo "Questionnaire ".$questionnaire_name." created successfully <br>";
+             } else {
+                 echo "Error: " . $add_questionnaire_sql . "<br>" . $link->error;
+             }
+         }
+
+         //This function creates new records in the question table in the database
+         //It will add the name of the questionnaire and each question in the questionnaire
+         function add_questions($questions_array, $questionnaire_name, $num_of_questions, $link)
+         {
+             //each question in the questions array will need to be added so the next bit of code is
+             //iterates through the array.
+             for($x = 1; $x <= $num_of_questions; $x++)
+             {
+                 //Store the sql statement in variable $add_question_sql
+                 $add_question_sql = "INSERT INTO questions VALUES ('".$questions_array[$x]."', '".$questionnaire_name."', '".$x."')";
+                 if ($link->query($add_question_sql) === TRUE)
+                 {
+                     echo $question_array[$x]."<br> added to questionnaire <br>";
+                 } else {
+                     echo "Error: " . $add_question_sql . "<br>" . $link->error;
+                 }
+             }
+         }
+
+         add_questionaire($questionnaire_name, $creator_name, $link);
+         add_questions($questions_array, $questionnaire_name, $num_of_questions, $link);
+
+
+         ?>
+
       <!-- Display the countdown timer in an element -->
       <p id="demo"></p>
 
