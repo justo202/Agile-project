4ae59603f94b9ac8a741941b3bca1970b48f8f49
Brenyn Goodwin <44468754+LupusSolus99@users.noreply.github.com>
2021-02-01 19:14:54 +0000

    Loading transcripts

---
+++ load.php
@@ -0,0 +1,23 @@
+<?php
+if(isset($_POST["read"]))
+{
+	$file = $_FILES["load"]["name"];
+	$fileError = $_FILES["load"]["error"];
+	
+	$fileRead = fopen($file, "r") or die("Unable to open file, please try again!");
+	//read line by line unto end of the file
+	while(!feof($fileRead))
+	{
+		if ($fileError === 0)
+		{
+			echo fgets($fileRead) . "<br>";
+		}
+		else
+		{
+			echo "Sorry there was an issue reading from file.";
+		}
+	}
+	fclose($fileRead);
+}
+
+?>
