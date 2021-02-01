7c2342e8edbd7cb7f1d16472afd93d9cdb248f2a
Brenyn Goodwin <44468754+LupusSolus99@users.noreply.github.com>
2021-02-01 21:00:23 +0000

    Saving transcripts

---
+++ save.php
@@ -0,0 +1,13 @@
+<?php
+if(isset($_POST["save"]))
+{
+	//user names the text file
+	$save = readline("Enter a file name: ");
+	$file = fopen("$save.txt", 'a+') or die("Unable to save, please try again later.");
+	//data writes here
+	$txt = "";
+	fwrite($file, $txt);
+	fclose($file);
+		
+}
+?>
