c071c7b4966cff92c4c9f33509b1e93d9009821d
vivienprk <77679471+vivienprk@users.noreply.github.com>
2021-02-02 17:02:53 +0100

    Update index.php

--- index.php
+++ index.php
@@ -117,6 +117,8 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
       </div>
 
       <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+      <button class="btn btn-lg btn-primary btn-block" type="submit">Sign up</button>
+
       <p class="mt-5 mb-3 text-muted">&copy; place holder</p>
     </form>
     </div>
