af7613793a1536f43eeb3705a86f79c9d95e5bca
Brenyn Goodwin <44468754+LupusSolus99@users.noreply.github.com>
2021-02-02 23:44:14 +0000

    Added some validation, with some bug fixes

--- load.php
+++ load.php
@@ -3,22 +3,32 @@ if(isset($_POST["load"]))
 {
 	$file = $_FILES["load"]["name"];
 	$fileError = $_FILES["load"]["error"];
+	$fileType = $_FILES["load"]["type"];
 	
-	$fileRead = fopen($file, "r") or die("Unable to open file, please try again!");
-	//read line by line unto end of the file
-	while(!feof($fileRead))
+	//seperate '.' from txt and file name 
+	$extension = explode(".", $file);
+	//convert the txt to lowercase if needed
+	$lowerExt = strtolower(end($extension));
+	$allowedExts = array("txt", "text/plain");
+	
+	if (in_array($lowerExt, $allowedExts));
 	{
-		if ($fileError === 0)
+		$fileRead = fopen($file, "r") or die("Unable to open file, please try again!");
+		//read line by line unto end of the file
+		while(!feof($fileRead))
 		{
-			echo fgets($fileRead) . "<br>";
+			if ($fileError === 0)
+			{
+				echo fgets($fileRead) . "<br>";
+			}
+			else
+			{
+				echo "Sorry there was an issue reading from file.";
+			}
 		}
-		else
-		{
-			echo "Sorry there was an issue reading from file.";
-		}
+		fclose($fileRead);
+		header("Location: transcript_gen_test.html?loadsuccessful");
 	}
-	fclose($fileRead);
-	header("Location: transcript_gen_test.html?loadsuccessful");
 }
 
 ?>
