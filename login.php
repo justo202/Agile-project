ef24d23d3f1a009205b9ceeac86544b86300842e
vivienprk <77679471+vivienprk@users.noreply.github.com>
2021-02-02 21:54:28 +0100

    Update login.php

--- login.php
+++ login.php
@@ -1,40 +1,100 @@
+<?php
+
+session_start();
+
+	include("db.php");
+	include("functions.php");
+
+
+	if($_SERVER['REQUEST_METHOD'] == "POST")
+	{
+		//something was posted
+		$user_name = $_POST['username'];
+		$password = $_POST['password'];
+
+		if(!empty($user_name) && !empty($password) && !is_numeric($user_name))
+		{
+
+			//read from database
+			$query = "select * from users where user_name = '$user_name' limit 1";
+			$result = mysqli_query($con, $query);
+
+			if($result)
+			{
+				if($result && mysqli_num_rows($result) > 0)
+				{
+
+					$user_data = mysqli_fetch_assoc($result);
+
+					if($user_data['password'] === $password)
+					{
+
+						$_SESSION['user_id'] = $user_data['user_id'];
+						header("Location: index.php");
+						die;
+					}
+				}
+			}
+
+			echo "wrong username or password!";
+		}else
+		{
+			echo "wrong username or password!";
+		}
+	}
+
+?>
+
+
 <!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title>Login Page</title>
-  </head>
-  <body>
-    <div id="frm">
-      <form action="process.php" method="POST">
-        <p>
-          <label>Username:</label>
-          <input type="text" id="user" name="user">
-        </p>
-        <p>
-          <label>Password:</label>
-          <input type="password" id="pass" name="pass">
-        </p>
-        <div>
-          <input type="radio" id="login" name="buttn" value="login" checked>
-          <label for="login">Log in</label>
-        </div>
-        <div>
-          <input type="radio" id="signup" name="buttn" value="signup">
-          <label for="signup">Sign up</label>
-        </div>
-        <p>
-          <button type="submit" onclick="myFunction()" id="Submit" name="btn" value="Submit" placeholder="Go!">
-        </p>
-      </form>
-    </div>
+<html>
+<head>
+	<title>Login</title>
+</head>
+<body>
 
-    <script>
-    function myFunction() {
-      var x = document.getElementById("login").checked;
-      document.getElementById("demo").innerHTML = x;
-    }
-    </script>
+	<style type="text/css">
 
-  </body>
+	#text{
+
+		height: 25px;
+		border-radius: 5px;
+		padding: 4px;
+		border: solid thin #aaa;
+		width: 100%;
+	}
+
+	#button{
+
+		padding: 10px;
+		width: 100px;
+		color: white;
+		background-color: lightblue;
+		border: none;
+	}
+
+	#box{
+
+		background-color: grey;
+		margin: auto;
+		width: 300px;
+		padding: 20px;
+	}
+
+	</style>
+
+	<div id="box">
+
+		<form method="post">
+			<div style="font-size: 20px;margin: 10px;color: white;">Login</div>
+
+			<input id="text" type="text" name="user_name"><br><br>
+			<input id="text" type="password" name="password"><br><br>
+
+			<input id="button" type="submit" value="Login"><br><br>
+
+			<a href="signup.php">Click to Signup</a><br><br>
+		</form>
+	</div>
+</body>
 </html>
