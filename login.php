991f9ace61cace7b71e5390240613883fbc165f7
gerardoblanco <gerardblanc.oh@gmail.com>
2021-02-02 18:17:22 +0100

    uncheck both buttons

--- login.php
+++ login.php
@@ -20,7 +20,7 @@
           <label for="login">Login</label>
         </p>
         <p>
-          <input type="radio" id="signup" name="signup" value="signup" checked>
+          <input type="radio" id="signup" name="signup" value="signup">
           <label for="signup">Sign up</label>
         </p>
         <p>
