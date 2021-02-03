1bf7f5fd2dd5803c6b1388a7a15a30e22a4c435b
Brenyn Goodwin <44468754+LupusSolus99@users.noreply.github.com>
2021-02-03 16:49:54 +0000

    some bug fixes

--- load.php
+++ load.php
@@ -9,25 +9,26 @@ if(isset($_POST["load"]))
 	$extension = explode(".", $file);
 	//convert the txt to lowercase if needed
 	$lowerExt = strtolower(end($extension));
+	
 	$allowedExts = array("txt", "text/plain");
 	
 	if (in_array($lowerExt, $allowedExts));
 	{
 		$fileRead = fopen($file, "r") or die("Unable to open file, please try again!");
-		//read line by line unto end of the file
-		while(!feof($fileRead))
-		{
 			if ($fileError === 0)
 			{
-				echo fgets($fileRead) . "<br>";
+				//read line by line unto end of the file
+				while(!feof($fileRead))
+				{
+					echo fgets($fileRead) . "<br>";
+				}		
+				fclose($fileRead);
+				header("Location: transcript_gen_test.html?loadsuccessful");
 			}
 			else
 			{
 				echo "Sorry there was an issue reading from file.";
 			}
-		}
-		fclose($fileRead);
-		header("Location: transcript_gen_test.html?loadsuccessful");
 	}
 	else
 	{	
