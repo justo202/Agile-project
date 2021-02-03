116770528afa1e46461e5327e400a45fcb68ea8c
gerardoblanco <gerardblanc.oh@gmail.com>
2021-02-03 19:34:06 +0100

    syntax error

--- process.php
+++ process.php
@@ -2,14 +2,6 @@
   include 'db.php';
   $username = $_POST['user'];
   $password = $_POST['pass'];
-//  $buttonvalue= $_POST['bttn'];
-
-//  if ($buttonvalue == signup) {
-    // code...
-//  }
-//  else {
-    // login code
-//  }
 
   $username = stripcslashes($username);
   $password = stripcslashes($password);
@@ -17,7 +9,7 @@
   $password = mysql_real_escape_string($password);
 
 
-  $sql = "SELECT * FROM `users` WHERE `username` = '$username' AND `password` = '$password'";
+  $result = "SELECT * FROM `users` WHERE `username` = '$username' AND `password` = '$password'";
   //$result = mysql_query("select * from users where username = '$username' and password = '$password'")
   //            or die("Failed to query database ".mysql_error());
   $row = mysql_fetch_array($result);
