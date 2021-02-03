81403376f083d8da51fd77987f7fad9544ea6ccf
gerardoblanco <gerardblanc.oh@gmail.com>
2021-02-03 19:14:08 +0100

    changed back to previous structure as it wasnt working

--- testlogin.php
+++ testlogin.php
@@ -5,8 +5,11 @@
     <title>Login Page</title>
   </head>
   <body>
+    <?php
+    include 'db.php';
+    ?>
     <div id="frm">
-      <form method="POST">
+      <form action="process.php" method="POST">
         <p>
           <label>Username:</label>
           <input type="text" id="user" name="user">
@@ -22,30 +25,3 @@
     </div>
   </body>
 </html>
-
-<?php
-  include 'db.php';
-  $username = $_POST['user'];
-  $password = $_POST['pass'];
-
-  $username = stripcslashes($username);
-  $password = stripcslashes($password);
-  $username = mysql_real_escape_string($username);
-  $password = mysql_real_escape_string($password);
-
-  if (empty($username)||empty($password)) {
-    echo "Failed to Login!";
-  }
-  else {
-    $sql = "SELECT * FROM `users` WHERE `username` = '$username' AND `password` = '$password'";
-    //$result = mysql_query("select * from users where username = '$username' and password = '$password'")
-    //            or die("Failed to query database ".mysql_error());
-    $row = mysql_fetch_array($result);
-    if ($row['username'] == $username && $row['password'] == $password) {
-      //echo '<h1><b>Login Succesfull '. $username .'!</b> </h1>';
-      header("Location: https://agile-project.azurewebsites.net/home.php");
-    } else {
-      echo "Failed to Login!";
-    }
-  }
- ?>
