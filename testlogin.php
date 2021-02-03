7f9d84afbfd1aea30bfe81b5f3b8c67b319e6fc4
gerardoblanco <gerardblanc.oh@gmail.com>
2021-02-03 19:05:52 +0100

    restructured code

--- testlogin.php
+++ testlogin.php
@@ -1,30 +1,4 @@
-<?php
-  include 'db.php';
-  $username = $_POST['user'];
-  $password = $_POST['pass'];
-
-  $username = stripcslashes($username);
-  $password = stripcslashes($password);
-  $username = mysql_real_escape_string($username);
-  $password = mysql_real_escape_string($password);
-
-  if (empty($username)||empty($password)) {
-    echo "Failed to Login!";
-  }
-  else {
-    $sql = "SELECT * FROM `users` WHERE `username` = '$username' AND `password` = '$password'";
-    //$result = mysql_query("select * from users where username = '$username' and password = '$password'")
-    //            or die("Failed to query database ".mysql_error());
-    $row = mysql_fetch_array($result);
-    if ($row['username'] == $username && $row['password'] == $password) {
-      echo '<h1><b>Login Succesfull '. $username .'!</b> </h1>';
-    } else {
-      echo "Failed to Login!";
-    }
-  }
- ?>
-
- <!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
@@ -48,3 +22,30 @@
     </div>
   </body>
 </html>
+
+<?php
+  include 'db.php';
+  $username = $_POST['user'];
+  $password = $_POST['pass'];
+
+  $username = stripcslashes($username);
+  $password = stripcslashes($password);
+  $username = mysql_real_escape_string($username);
+  $password = mysql_real_escape_string($password);
+
+  if (empty($username)||empty($password)) {
+    echo "Failed to Login!";
+  }
+  else {
+    $sql = "SELECT * FROM `users` WHERE `username` = '$username' AND `password` = '$password'";
+    //$result = mysql_query("select * from users where username = '$username' and password = '$password'")
+    //            or die("Failed to query database ".mysql_error());
+    $row = mysql_fetch_array($result);
+    if ($row['username'] == $username && $row['password'] == $password) {
+      //echo '<h1><b>Login Succesfull '. $username .'!</b> </h1>';
+      header("Location: https://agile-project.azurewebsites.net/home.php");
+    } else {
+      echo "Failed to Login!";
+    }
+  }
+ ?>
