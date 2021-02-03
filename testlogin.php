bc42967b0a9081bbf44ec0e20b1e614e49417773
gerardoblanco <gerardblanc.oh@gmail.com>
2021-02-03 19:03:10 +0100

    added if statement logic for empty values

--- testlogin.php
+++ testlogin.php
@@ -8,17 +8,20 @@
   $username = mysql_real_escape_string($username);
   $password = mysql_real_escape_string($password);
 
-
-  $sql = "SELECT * FROM `users` WHERE `username` = '$username' AND `password` = '$password'";
-  //$result = mysql_query("select * from users where username = '$username' and password = '$password'")
-  //            or die("Failed to query database ".mysql_error());
-  $row = mysql_fetch_array($result);
-  if ($row['username'] == $username && $row['password'] == $password) {
-    echo '<h1><b>Login Succesfull '. $username .'!</b> </h1>';
-  } else {
+  if (empty($username)||empty($password)) {
     echo "Failed to Login!";
   }
-
+  else {
+    $sql = "SELECT * FROM `users` WHERE `username` = '$username' AND `password` = '$password'";
+    //$result = mysql_query("select * from users where username = '$username' and password = '$password'")
+    //            or die("Failed to query database ".mysql_error());
+    $row = mysql_fetch_array($result);
+    if ($row['username'] == $username && $row['password'] == $password) {
+      echo '<h1><b>Login Succesfull '. $username .'!</b> </h1>';
+    } else {
+      echo "Failed to Login!";
+    }
+  }
  ?>
 
  <!DOCTYPE html>
