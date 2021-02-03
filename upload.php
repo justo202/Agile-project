5c865bab406c130fd71246c1c4215df6075314dc
Brenyn Goodwin <44468754+LupusSolus99@users.noreply.github.com>
2021-02-03 19:32:02 +0000

    minor changes

--- upload.php
+++ upload.php
@@ -27,7 +27,7 @@ if (isset($_POST['submit']))
 			//creates a new id 
 			$fileNewID = uniqid('', true).".".$lowerExt;
 			//the file path that the file will be moved to
-			$dir = 'uploads/'.$fileTmp;
+			$dir = 'uploads/'.$file;
 			//transfering file
 			move_uploaded_file($fileTmp, $dir);
 			if (is_uploaded_file($fileTmp))
