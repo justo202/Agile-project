91b1b185c3927f87d8fe043328aa01b7c5d8f6e6
gerardoblanco <gerardblanc.oh@gmail.com>
2021-02-03 20:41:40 +0100

    debugging

--- register.php
+++ register.php
@@ -65,15 +65,15 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     if(empty($username_err) && empty($password_err) && empty($confirm_password_err)){
 
         // Prepare an insert statement
-        $sql = "INSERT INTO users (username, password) VALUES (?, ?)";
+        $sql = "INSERT INTO users (username, password) VALUES ("hello", "mememe")";
 
-        if($stmt = mysqli_prepare($link, $sql)){
+        /*if($stmt = mysqli_prepare($link, $sql)){
             // Bind variables to the prepared statement as parameters
             mysqli_stmt_bind_param($stmt, "ss", $param_username, $param_password);
 
             // Set parameters
             $param_username = $username;
-            $param_password = $password; // Creates a password hash
+            $param_password = password_hash($password, PASSWORD_DEFAULT); // Creates a password hash
 
             // Attempt to execute the prepared statement
             if(mysqli_stmt_execute($stmt)){
@@ -86,6 +86,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             // Close statement
             mysqli_stmt_close($stmt);
         }
+        */
     }
 
     // Close connection
