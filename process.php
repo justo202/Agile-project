216968edda0ccdcaf92561c611f930cfd2feb3b5
gerardoblanco <gerardblanc.oh@gmail.com>
2021-02-03 19:41:30 +0100

    test changes

--- process.php
+++ process.php
@@ -9,14 +9,35 @@
   $password = mysql_real_escape_string($password);
 
 
-  $result = "SELECT * FROM `users` WHERE `username` = '$username' AND `password` = '$password'";
+  $sql = "SELECT * FROM `users` WHERE `username` = '$username' AND `password` = '$password'";
   //$result = mysql_query("select * from users where username = '$username' and password = '$password'")
   //            or die("Failed to query database ".mysql_error());
-  $row = mysql_fetch_array($result);
+  /*$row = mysql_fetch_array($result);
   if ($row['username'] == $username && $row['password'] == $password) {
     echo '<h1><b>Login Succesfull '. $username .'!</b> </h1>';
   } else {
     echo "Failed to Login!";
+  }*/
+
+
+  if($result = mysqli_query($link, $sql)){
+      if(mysqli_num_rows($result) > 0){
+        $val = 1;
+          while($row = mysqli_fetch_array($result)){
+
+            if ($row['username'] == $username && $row['password'] == $password) {
+              echo '<h1><b>Login Succesfull '.$username.'!</b> </h1>';
+            } else {
+              echo "Failed to Login!";
+            }
+
+          }
+          echo "<input type = \"hidden\" value = \"".$name."\" name = \"name\">";
+          // Free result set
+          mysqli_free_result($result);
+      } else{
+          echo "No records matching your query were found.";
+      }
   }
 
  ?>
