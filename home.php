3f86eb885978e797fca91616b6ec4bb7a91a6685
gerardoblanco <gerardblanc.oh@gmail.com>
2021-02-04 16:17:03 +0100

    fixing logout for home page

--- home.php
+++ home.php
@@ -21,7 +21,7 @@ session_start();
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
-        <a class="navbar-brand" href="index.php">Log out</a>
+        <a class="navbar-brand" href="logout.php">Log out</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
