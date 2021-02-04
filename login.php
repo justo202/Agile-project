c69d18206ed96b36b4a8e05917db181851714f1e
vivienprk <77679471+vivienprk@users.noreply.github.com>
2021-02-04 19:02:25 +0100

    Update login.php

--- login.php
+++ login.php
@@ -62,6 +62,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                             $_SESSION["loggedin"] = true;
                             $_SESSION["id"] = $id;
                             $_SESSION["username"] = $username;
+                            $_SESSION["roleID"] = $roleID;
 
                             // Redirect user to welcome page
                             header("location: welcome.php");
