61a921c642f28c40399f7403cabf21460da3ea35
gerardoblanco <gerardblanc.oh@gmail.com>
2021-02-04 22:27:30 +0100

    removed ethics form link so that it only appears on home page for researchers

--- Form.php
+++ Form.php
@@ -22,12 +22,7 @@
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
               <span class="navbar-toggler-icon"></span>
             </button>
-
-            <a class="navbar-brand" href="EthicsForm.html">Ethics Form</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon"></span>
-            </button>
-            <a class="navbar-brand" href="index.php">Log out</a>
+            <a class="navbar-brand" href="logout.php">Log out</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
               <span class="navbar-toggler-icon"></span>
             </button>
