e46a51b19122aff9ed690d2388eadb334a7e9bdf
Brenyn Goodwin <44468754+LupusSolus99@users.noreply.github.com>
2021-02-04 19:11:58 +0000

    added other pages to the nav bar

--- transcript.php
+++ transcript.php
@@ -9,14 +9,28 @@
   <body>
     <nav class="navbar navbar-expand-lg navbar-dark" style="background-color:#343a40">
       <div class="container-fluid">
-        <a class="navbar-brand" href="#" style="font-size:30px; font-weight: bold;">Home</a>
+        <a class="navbar-brand" href="home.php" style="font-size:20px; font-weight: bold;">Home</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
+		<a class="navbar-brand" href="EthicsForm.html" style="font-size:20px; font-weight: bold;">Ethics Form</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+			<a class="navbar-brand" href="transcript.php">Transcript</a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<a class="navbar-brand" href="logout.php">Log out</a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			
         <div class="collapse navbar-collapse" id="navbarNav">
           <ul class="navbar-nav">
             <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#"style="font-size:20px; font-weight: bold;">Make Transcript</a>
+     
+			  
             </li>
           </ul>
         </div>
