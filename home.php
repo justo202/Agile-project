726a49d7eb9603c7ba6fcacc6cc3f2540a154eff
vivienprk <77679471+vivienprk@users.noreply.github.com>
2021-02-04 19:35:10 +0100

    Update home.php
    back to normal

--- home.php
+++ home.php
@@ -8,7 +8,6 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
 
 ?>
 
-
 <!doctype html>
 <html lang="en">
   <head>
@@ -26,16 +25,10 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
-
-        <?php
-        if $_SESSION["roleID"] == 2 {
-        echo <a class="navbar-brand" href="EthicsForm.html">Ethics Form</a>
+        <a class="navbar-brand" href="EthicsForm.html">Ethics Form</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
-      }
-        ?>
-
         <a class="navbar-brand" href="logout.php">Log out</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
