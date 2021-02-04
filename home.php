03d552b2841ed7a16c7aedc6080ef36953b3ac56
gerardoblanco <gerardblanc.oh@gmail.com>
2021-02-04 16:24:22 +0100

    fixed session issue where you can still access home.php after log out

--- home.php
+++ home.php
@@ -1,6 +1,10 @@
 <?php
 session_start();
-  $_SESSION;
+// Check if the user is logged in, if not then redirect him to login page
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    header("location: login.php");
+    exit;
+}
 
 ?>
 
