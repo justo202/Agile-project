1220317628eff59827a764848313fb4beb4b6507
gerardoblanco <gerardblanc.oh@gmail.com>
2021-02-04 20:10:11 +0100

    role debugging

--- home.php
+++ home.php
@@ -5,6 +5,10 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
     header("location: login.php");
     exit;
 }
+elseif (!isset($_SESSION["role"]) || $_SESSION["role"] !== "1") {
+  header("location: participant_home.php");
+  exit;
+}
 
 ?>
 
