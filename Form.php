b846078161fca30fec4742a376097c7e945e37e9
justo202 <justaslabeikis23@gmail.com>
2021-02-05 12:22:02 +0200

    Update Form.php

--- Form.php
+++ Form.php
@@ -62,10 +62,11 @@
                     foreach($answ as $a)
                     {
                       echo " <div class=\"form-check\">";
-                      echo " <input class=\"form-check-input\" type=\"radio\" id = \"".$a."\"name=\"".$a."[]\" value=\"".$a."\" checked>";
+                      echo " <input class=\"form-check-input\" type=\"radio\" id = \"".$a."\"name=\"answers[]\" value=\"".$a."\" checked>";
                       echo "<label class=\"form-check-label\" for=\"".$a."\">".$a."</label>";
-                      echo "</div>";
+
                     }
+                    echo "</div>";
                     echo "<input type = \"hidden\" value = \"".$row['Question_Number']."\" name = \"num[]\">";
                     echo "</div>";
                   }
